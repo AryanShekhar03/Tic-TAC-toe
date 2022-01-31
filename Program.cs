0cd8d859fc3f91176503ea93a9923cbf16ebb33b
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-01-31 19:25:57 +0530

    Aaryan Added UC1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tic_tac
+{
+     class Program
+    {
+
+        static void Main(string[]args)
+        {
+            char[] Board;
+            Tictactoe obj = new Tictactoe();
+            Board = obj.Board();
+            
+        }
+    }
+}
---
+++ Tictactoe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tic_tac
+{
+    class Tictactoe
+    {
+        public char[] Board() 
+        { 
+            char[] Board = new char[10];
+            for (int i = 1; i < Board.Length; i++)
+            {
+                Board[i] = ' ';
+            }
+            Console.WriteLine("Board initialized");
+            return Board;
+        }
+    }
+}
